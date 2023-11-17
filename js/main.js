673f9961d7b42ed8509020ddef2e9e7a52fd98b0
Daniyar4ik <148819547+Daniyar4ik@users.noreply.github.com>
2023-11-17 20:42:21 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.reviews__slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+	prevArrow: "<img src='../img/arrow-left.svg' class='prev' alt='1'>",
+  nextArrow: "<img src='../img/arrow-right.svg' class='next' alt='2'>",
+});
+
